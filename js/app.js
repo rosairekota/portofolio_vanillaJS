ee44203dca2e9762ab57ed94d41d027fe137f4fc
rkota <rosairekota@gmail.com>
2021-07-20 22:09:32 +0100

    feat:add responsive and scroll buttom

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
 const headerContainer = document.querySelector("#header");
+const btnToggle = document.querySelector("#btnToggle");
+const navbarMenu = document.querySelector("#nav");
+const menuItems = document.querySelectorAll(".navbar a");
+const scrollBtn = document.querySelector("#scrollBtn");
 
-const handleScroll = (e) => {
+const handelScroll = (e) => {
   e.preventDefault();
   headerContainer.classList.toggle("sticky", window.scrollY > 440);
   headerContainer.classList.toggle("bevhor");
 };
-window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handelScroll);
+
+// onToggle
+const handelToggle = (e) => {
+  e.preventDefault();
+  btnToggle.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
+};
+btnToggle.addEventListener("click", handelToggle);
+
+// removeActiveClass
+const removeActiveClass = (e) => {
+  //e.preventDefault();
+  btnToggle.classList.remove("active");
+  navbarMenu.classList.remove("active");
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", removeActiveClass);
+});
+const handelToUpScroll = () => {
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+};
+
+window.addEventListener("scroll", handelToUpScroll);
+
+const scrollUp = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+scrollBtn.addEventListener("click", scrollUp);
